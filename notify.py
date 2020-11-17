638e8f96d69a6b0a2a22bd32be110d769f0a56a5
CORP\qiyuxing <3257268269@qq.com>
2020-11-17 10:56:48 +0800

    no message

--- notify.py
+++ notify.py
@@ -1,90 +1,70 @@
+'''
+  模板测试号注册地址：https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login
+'''
 # coding=utf-8
-import json
-import requests
+import json, requests
 
 class Notify:
-    def __init__(self, wechat_config):
-        self.appid = wechat_config['appid']
-        self.appsecret = wechat_config['appsecret']
-        self.template_id = wechat_config['template_id']
+    def __init__(self, appid = '', appsecret = '', template_id = ''):
+        self.appid = appid
+        self.appsecret = appsecret
+        self.template_id = template_id
         self.access_token = ''
 
+        self.get_access_token()
+
     # 获取access_token
-    def get_access_token(self, appid, appsecret):
-        url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (appid, appsecret)
+    def get_access_token(self):
+        url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (self.appid, self.appsecret)
+        self.access_token = json.loads(requests.get(url).text)['access_token']
 
-        response = requests.get(url)
-        json_data = response.text
-        data = json.loads(json_data)
-        access_token = data['access_token']
-        self.access_token = access_token
-        return self.access_token
-
-    # 获取用户列表
-    def get_user_list(self):
+    # 获取用户列表(关注该测试号的用户的openid)
+    def get_openids(self):
         if self.access_token == '':
-            self.get_access_token(self.appid, self.appsecret)
+            self.get_access_token()
 
-        access_token = self.access_token
-        url = 'https://api.weixin.qq.com/cgi-bin/user/get?access_token=%s&next_openid=' % str(access_token)
-        response = requests.get(url)
-        result = response.text
-        return json.loads(result)
+        url = 'https://api.weixin.qq.com/cgi-bin/user/get?access_token=%s&next_openid=' % str(self.access_token)
+        return json.loads(requests.get(url).text)['data']['openid']
 
-    def send_msg(self, openid, template_id, notify_msg):
+    def send_msg(self, openid, notify_msg):
         msg = {
             'touser': openid,
-            'template_id': template_id,
+            'template_id': self.template_id,
             #'url':'',#点击详情会跳转至的页面
-            'data':notify_msg
+            'data': notify_msg
         }
-
-        json_data = json.dumps(msg)
-        self.access_token = self.get_access_token(self.appid, self.appsecret)
-        access_token = self.access_token
-        url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s' % str(access_token)
-
-        print(json_data)
-        response = requests.post(url, data = json_data)
-        result = response.text
-        
-        print(result)
-        
+        if self.access_token == '':
+            self.get_access_token()
+        url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s' % str(self.access_token)
+        result = requests.post(url, data = json.dumps(msg)).text
         return json.loads(result)
 
-    # 发送消息
+
     def notify(self, msg, openids = []):
         if openids == []:
-            result = self.get_user_list()
-            openids = result['data']['openid']
+            openids = self.get_openids()
         for openid in openids:
-            result = self.send_msg(openid, self.template_id, msg)
+            result = self.send_msg(openid, msg)
             if result['errcode'] == 0:
-                print(' [INFO] send to %s is success' % openid)
+                print('OK')
             else:
-                print(' [ERROR] send to %s is error' % openid)
+                print('ERROR')
 
 
 if __name__ == '__main__':
+
     # 微信配置
-    wechat_config = {
-        'appid': '111',
-        'appsecret': '111',
-        'template_id': "111"
-    }
+    appid = 'wx9f6ac9208a3ea223'
+    appsecret = 'd178054816bdc7f756a3eb5cc5744abb'
+    template_id = 'pdHHR6IKjVoTQVTyUR_mBhjEzsXifE6FPbd5zN6dbQU'
 
-    # 接收者
-    openids = ['111']
-
-
-    ##################程序异常通知###########################
-    test_msg_2 = {
+    test_msg = {
         'key_1':{
             "value":"2019-1 11:13:23:48",
             'color': '#FF0000'
         },
         'key_2':{
-            "value":"【123】",
+            "value":"【数据异常】",
             'color': '#FF0000'
         },
         'key_3': {
@@ -97,5 +77,5 @@ if __name__ == '__main__':
         }
     }
 
-    notify = Notify(wechat_config)
-    notify.notify(test_msg_2)
+    nt = Notify(appid, appsecret, template_id)
+    nt.notify(test_msg)
